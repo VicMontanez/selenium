08167332aed70b67f067cd9cabb6e386a5210529
unknown <v_montanez@hotmail.com>
2020-05-15 01:34:40 -0400

    fixed annotations

--- selenium-for-beginners/src/test/java/com/herokuapp/internet/PositiveTests.java
+++ selenium-for-beginners/src/test/java/com/herokuapp/internet/PositiveTests.java
@@ -2,9 +2,10 @@ package com.herokuapp.internet;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class PositiveTests {
-//	@Test
+	@Test
 	public void loginTest() {
 		System.out.println("TESTING LOGIN");
 		
@@ -12,13 +13,15 @@ public class PositiveTests {
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
+		//maximize browser window
+		driver.manage().window().maximize();
+		
 		//open test page
-		String url = "https://the-internet.herokuapp.com/login";
+		String url = "http://the-internet.herokuapp.com/login";
 		driver.get(url);
 		System.out.println("Page is opened");
 		
-		//maximize browser window
-		driver.manage().window().maximize();
+
 		
 		// sleep for 3 seconds
 //		sleep(3000);
