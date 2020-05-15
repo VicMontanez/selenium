7b9c4dfb31c49364b3c1f775fb3a1e2b256c1ce1
unknown <v_montanez@hotmail.com>
2020-05-15 00:40:33 -0400

    added url to url variable

--- selenium-for-beginners/src/test/java/com/herokuapp/internet/PositiveTests.java
+++ selenium-for-beginners/src/test/java/com/herokuapp/internet/PositiveTests.java
@@ -12,7 +12,8 @@ public class PositiveTests {
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
-		//open test page 
+		//open test page
+		String url = "https://the-internet.herokuapp.com/login";
 		driver.get(url);
 		
 		// sleep for 3 seconds
