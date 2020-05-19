72e10fb9ac889feb6fcef41394717e1237c194ed
unknown <v_montanez@hotmail.com>
2020-05-18 21:09:28 -0400

    added password field

--- selenium-for-beginners/src/test/java/com/herokuapp/internet/PositiveTests.java
+++ selenium-for-beginners/src/test/java/com/herokuapp/internet/PositiveTests.java
@@ -34,7 +34,7 @@ public class PositiveTests {
 		
 		//enter password
 		WebElement password = driver.findElement(By.name("password"));
-		
+		password.sendKeys("SuperSeceretPassword");
 		//click login button
 		WebElement loginButton = driver.findElement(By.tagName("button"));
 		
