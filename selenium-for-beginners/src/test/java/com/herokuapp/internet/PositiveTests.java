6ef2c78645e0f6a613a75aaffa48d0c501d4e5d4
unknown <v_montanez@hotmail.com>
2020-05-19 23:11:58 -0400

    used assertTrue to test message after login

--- selenium-for-beginners/src/test/java/com/herokuapp/internet/PositiveTests.java
+++ selenium-for-beginners/src/test/java/com/herokuapp/internet/PositiveTests.java
@@ -11,54 +11,56 @@ public class PositiveTests {
 	@Test
 	public void loginTest() {
 		System.out.println("TESTING LOGIN");
-		
+
 		// Create driver
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
-		//maximize browser window
+
+		// maximize browser window
 		driver.manage().window().maximize();
-		
-		//open test page
+
+		// open test page
 		String url = "http://the-internet.herokuapp.com/login";
 		driver.get(url);
 		System.out.println("Page is opened");
-		
 
-		
 		// sleep for 3 seconds
 //		sleep(3000);
-		
-		//enter username
+
+		// enter username
 		WebElement username = driver.findElement(By.id("username"));
 		username.sendKeys("tomsmith");
-		
-		//enter password
+
+		// enter password
 		WebElement password = driver.findElement(By.name("password"));
 		password.sendKeys("SuperSecretPassword!");
-		
-		//click login button
+
+		// click login button
 		WebElement loginButton = driver.findElement(By.tagName("button"));
 		loginButton.click();
-		
-		//verifications
+
+		// verifications
 		String expectedUrl = "http://the-internet.herokuapp.com/secure";
 		String actualUrl = driver.getCurrentUrl();
 		Assert.assertEquals(actualUrl, expectedUrl, "Actual page url is not the same as expected");
-		
-		//logout button
+
+		// logout button
 		WebElement logoutButton = driver.findElement(By.xpath("//a[@class= 'button secondary radius']"));
 		Assert.assertTrue(logoutButton.isDisplayed(), "Log Out button is not visible");
-		
-		//successful login message
+
+		// successful login message
 		WebElement successMessage = driver.findElement(By.cssSelector("#flash"));
 		String expectedMessage = "You logged into a se3cure area";
 		String actualMessage = successMessage.getText();
-		Assert.assertEquals(actualMessage, expectedMessage, "Actual message is not the same as expected");
-		
+		// Assert.assertEquals(actualMessage, expectedMessage, "Actual message is not
+		// the same as expected");
+		Assert.assertTrue(actualMessage.contains(expectedMessage),
+				"Actual message does not contain expected message.\nActual Message: " + actualMessage
+						+ "\nExpected Message: " + expectedMessage);
+
 		// close browser
 		driver.quit();
-		
+
 	}
 
 }
